a3d68e53e910d53cad23742deebd70e23a442697
ingridvulpix <ingridalmeidafs@gmail.com>
2022-03-06 16:04:08 -0300

    Imprime todas as matérias

--- grade.py
+++ grade.py
@@ -1,4 +1,4 @@
-from curriculo_materias import disciplinas
+from curriculo_materias import grade_curricular
 
 #TO DO:Imprimir a grade de cada período
 #TO DO:Pegar Input do terminal
@@ -6,13 +6,13 @@ from curriculo_materias import disciplinas
 #TO DO:Identificar a semana do cronograma atrás do input do usuário
 
 class Grade:
-    def __init__(self,periodo,verifica):
+    def __init__(self,periodo):
         self.periodo = periodo
-        self.verifica = verifica
+ 
+    def mostrar_materias(self):#mostra todas as matérias do curriculo
+        for materia in grade_curricular:
+            print('\nNome da materia: ', materia)
 
-          
-    def mostra_materias(periodo):#materias para determinado periodo
-        print(disciplinas[periodo])
 
-p1 = Grade(1,0)
-print(p1.mostra_materias())
+p1 = Grade('1')
+print(p1.mostrar_materias())
