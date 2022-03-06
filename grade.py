e006ff4954d7b5346a51036e24e8eb21be890750
ingridvulpix <ingridalmeidafs@gmail.com>
2022-03-06 19:21:35 -0300

    Materias do periodo

--- grade.py
+++ grade.py
@@ -1,19 +1,22 @@
 from curriculo_materias import grade_curricular
 import pandas as pd
 
-#TO DO:Imprimir a grade de cada período
 #TO DO:Colocar cronograma de cada disciplina
 #TO DO:Identificar a semana do cronograma atrás do input do usuário
-#TO DO:Corrigir laço for e mostrar_materias, ultimo None
 
 class Grade:
     def __init__(self,periodo):
         self.periodo = periodo
- 
-    def mostrar_materias(self):#mostra todas as matérias do curriculo
+        global df 
         df = pd.DataFrame.from_dict(grade_curricular, orient='index')
+
+    def mostrar_materias(self):#mostra todas as matérias do curriculo
         print(df)
+    def materias_periodo(self,periodo):#mostra matérias do período selecionado
+        result_df = df.loc[df['periodo']== periodo]
+        print(result_df)
 
 
-p1 = Grade('1')
-print(p1.mostrar_materias())
+#p1 = Grade(1)
+#p1.mostrar_materias()
+#p1.materias_periodo(5)
