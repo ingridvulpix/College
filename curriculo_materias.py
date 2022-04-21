fc895a4c11a153df91041469d052e9c79e226a26
ingridvulpix <ingridalmeidafs@gmail.com>
2022-04-21 18:59:24 -0300

    adicionando descricao das disciplinas

--- curriculo_materias.py
+++ curriculo_materias.py
@@ -27,3 +27,33 @@ grade_curricular = {
     'Arquitetura e Projeto de Sistemas':{'periodo':6,'sigla':'APS','horas':80,'requisito':'AS'},
     'Computação Gráfica':{'periodo':6,'sigla':'CG','horas':80,'requisito':'PIG, ALGLIN'}
     }
+
+descricao_disciplina = {
+    'Matemática básica':{}, 
+    'Construção de páginas da web':{},
+    'Inglês Instrumental':{},
+    'Introdução a Informática':{},
+    'Projeto e Desenvolvimento de Algortimos':{},
+    'Fundamentos de Algoritmos de Computação':{},
+    'Organização de Computadores':{},
+    'Algebra Linear':{},
+    'Fundamentos de Programação':{},
+    'Matemática para Computação':{},
+    'Física para Computação':{},
+    'Estrutura de Dados':{},
+    'Programação com Interfaces Gráficas':{},
+    'Probabilidade e Estatística':{},
+    'Modelagem de Informação':{},
+    'Sistemas Operacionais':{},
+    'Banco de Dados':{},
+    'Programação Orientada a Objetos':{},
+    'Engenharia de Software':{},
+    'Redes de Computadores I':{},
+    'Análise de Sistemas':{},
+    'Programação de Aplicações Web':{},
+    'Empreendedorismo e Ética':{},
+    'Tese de Conclusão de Curso':{},
+    'Redes de Computadores II':{},
+    'Arquitetura e Projeto de Sistemas':{},
+    'Computação Gráfica':{}
+}
